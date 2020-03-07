94776fe800524fb59417a2d64038c7f47dfc48a2
Osman Forhad <osmanforhad93@gmail.com>
2020-03-07 15:30:38 +0600

    run spring boot application using RestController

--- src/main/java/com/lms/controllers/rest/MainRestController.java
+++ src/main/java/com/lms/controllers/rest/MainRestController.java
@@ -1,5 +1,16 @@
 package com.lms.controllers.rest;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController /**define rest controller annotation*/
 public class MainRestController {
 
+	@GetMapping(value = "/")/**define which request this method will handle*/
+	public String hello() {
+		
+		return "Spring Boot Application using MainRestController";
+		
+	}//end of the hello method
+	
 }//end of the MainRestController
