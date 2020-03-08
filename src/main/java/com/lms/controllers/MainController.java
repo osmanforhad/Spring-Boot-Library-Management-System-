a7eec5b3718b37e376a576bafeffd7127f96f739
Osman Forhad <osmanforhad93@gmail.com>
2020-03-08 13:39:15 +0600

    correction of application properties and writing method for display index jsp page

--- src/main/java/com/lms/controllers/MainController.java
+++ src/main/java/com/lms/controllers/MainController.java
@@ -1,5 +1,14 @@
 package com.lms.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller /**mention annotation the class works as a controller*/
 public class MainController {
+	
+	@GetMapping("/hellojsp")/**Mapping the url name*/
+	public String init() {
+		return "index";/**return targated page*/
+	}//end of the init method
 
 }//end of the MainController class
