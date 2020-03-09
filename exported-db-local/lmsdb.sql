5648623332aedb36eb69b511f8452b72b16c9498
Osman Forhad <osmanforhad93@gmail.com>
2020-03-09 15:18:28 +0600

    add exported db local in project

---
+++ exported-db-local/lmsdb.sql
@@ -0,0 +1,58 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Mar 09, 2020 at 09:17 AM
+-- Server version: 5.7.24
+-- PHP Version: 7.2.19
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `lmsdb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `lms_tbl`
+--
+
+CREATE TABLE `lms_tbl` (
+  `id` int(11) NOT NULL,
+  `book_name` varchar(45) DEFAULT NULL,
+  `author` varchar(45) DEFAULT NULL,
+  `purchase_date` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `lms_tbl`
+--
+
+INSERT INTO `lms_tbl` (`id`, `book_name`, `author`, `purchase_date`) VALUES
+(1, 'JAVA', 'Tushar Gerose', '2020-03-09');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `lms_tbl`
+--
+ALTER TABLE `lms_tbl`
+  ADD PRIMARY KEY (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
