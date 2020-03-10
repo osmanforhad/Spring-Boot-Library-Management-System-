5c8e97fd68cad06fe6729f9eb425325f5142318e
Osman Forhad <osmanforhad93@gmail.com>
2020-03-10 11:52:10 +0600

    extends crudRepo with created interface and passing Book model as paramiter

--- src/main/java/com/lms/dao/LmsRepository.java
+++ src/main/java/com/lms/dao/LmsRepository.java
@@ -1,5 +1,11 @@
 package com.lms.dao;
 
-public interface LmsRepository {
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.lms.models.Book;
+
+@Repository
+public interface LmsRepository extends CrudRepository<Book, Long>{
 
 }//end of the interface
