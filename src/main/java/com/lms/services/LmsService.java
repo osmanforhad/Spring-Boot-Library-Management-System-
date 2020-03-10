4698b44c6f47626c71eed068bfe44460668696fc
Osman Forhad <osmanforhad93@gmail.com>
2020-03-10 12:14:06 +0600

    writing code for service class

--- src/main/java/com/lms/services/LmsService.java
+++ src/main/java/com/lms/services/LmsService.java
@@ -1,5 +1,43 @@
 package com.lms.services;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.lms.dao.LmsRepository;
+import com.lms.models.Book;
+
+@Service /**annoted this class as service*/
 public class LmsService {
+	
+	/**
+	 * create object of repository for 
+	 * access the data from dao
+	 * */
+	@Autowired /**use Autowired annotation for dependency injection*/
+	private LmsRepository lmsRepository;
+	
+	/**
+	 * Create  method findAllBooks for 
+	 * return collection
+	 * */
+	public Collection<Book> findAllBooks(){
+		
+		/**create a List name
+		 * as books
+		 * */
+		List<Book> books = new ArrayList<Book>(); /**here Book is my created model*/
+		
+		/**write a for loop*/
+		for(Book book : lmsRepository.findAll()) {
+			books.add(book); //add collection list
+		}//end of the for loop
+		
+		return books;
+		
+	}//end of the findAllBooks method
 
 }//end of the service class
